2084020737f16580a1e72c521f0bcd726b43c8af
DianaC0417 <164435207+DianaC0417@users.noreply.github.com>
2026-04-30 17:05:00 -0400

    Update Customization.tsx

--- src/ui/pages/Customization.tsx
+++ src/ui/pages/Customization.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable */
 //src/ui/pages/Customization.tsx
-//import React, { useState } from 'react';
+//import React
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom'; // Importamos el navegador
 
 const Customization = () => {
